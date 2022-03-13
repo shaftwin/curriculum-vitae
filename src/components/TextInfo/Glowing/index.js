cc2ee991a5655c2f8d0090fb1ab46911880996cd
Shaftwin <florian.champin.neault@gmail.com>
2022-03-13 20:51:29 +0100

    fix: default glowing letters

--- src/components/TextInfo/Glowing/index.js
+++ src/components/TextInfo/Glowing/index.js
@@ -1,14 +1,28 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { GlowingContainer, CustomLi, HiddenInput, Letter } from './styled';
 
 const Glowing = ({ title, className }) => {
+  const [checked, setChecked] = useState(() =>
+    title.split('').map((letter, idx) => !(idx % 2)),
+  );
+
+  const updateGlowing = (idx) => {
+    const newArr = [...checked];
+    newArr[idx] = !checked[idx];
+    setChecked(newArr);
+  };
+
   return (
     <GlowingContainer className={className}>
       {title.split('').map((letter, idx) => {
         return (
           <CustomLi key={idx}>
-            <HiddenInput type="checkbox" />
+            <HiddenInput
+              onClick={() => updateGlowing(idx)}
+              checked={checked[idx]}
+              type="checkbox"
+            />
             <Letter>{letter}</Letter>
           </CustomLi>
         );
