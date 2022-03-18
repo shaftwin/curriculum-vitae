4a5846ba3479d402567fa0f78f6d440912c2caa0
Shaftwin <florian.champin.neault@gmail.com>
2022-03-18 23:30:58 +0100

    fix: jobs animation

--- src/domains/home/pages/Jobs/index.jsx
+++ src/domains/home/pages/Jobs/index.jsx
@@ -1,101 +1,77 @@
 /* eslint-disable max-len */
 import React, { useState } from 'react';
-import { Container, Grid, CustomJobCard } from './styled';
+import {
+  Container,
+  Grid,
+  CustomJobCard,
+  JobModal,
+  ColumnContainer,
+} from './styled';
 
 const Jobs = () => {
-  const [shouldExpand, setShouldExpand] = useState([
-    false,
-    false,
-    false,
-    false,
-  ]);
-  const [shouldHide, setShouldHide] = useState([false, false, false, false]);
-
-  const reset = () => {
-    setShouldExpand([false, false, false, false]);
-    setShouldHide([false, false, false, false]);
-  };
+  const [displayModal, setDisplayModal] = useState(undefined);
+  const [modalColor, setModalColor] = useState(null);
 
   return (
     <Container>
       <Grid>
-        <CustomJobCard
-          onClick={() => {
-            if (shouldExpand[0]) return reset();
-            setShouldExpand([
-              !shouldExpand[0],
-              shouldExpand[1],
-              shouldExpand[2],
-              shouldExpand[3],
-            ]);
-            setShouldHide([false, true, true, true]);
-            return null;
-          }}
-          color="#8BF896"
-          companyName="Tekyn"
-          jobTitle="Lead Developer"
-          jobDescription="Lots of work done"
-          shouldExpand={shouldExpand[0]}
-          shouldHide={shouldHide[0]}
-        />
-        <CustomJobCard
-          onClick={() => {
-            if (shouldExpand[1]) return reset();
-            setShouldExpand([
-              shouldExpand[0],
-              !shouldExpand[1],
-              shouldExpand[2],
-              shouldExpand[3],
-            ]);
-            setShouldHide([true, false, true, true]);
-            return null;
-          }}
-          color="#e4ee89"
-          companyName="TrackTL"
-          jobTitle="Frontend Developer"
-          jobDescription="Lots of work done"
-          shouldExpand={shouldExpand[1]}
-          shouldHide={shouldHide[1]}
-        />
-        <CustomJobCard
-          onClick={() => {
-            if (shouldExpand[2]) return reset();
-            setShouldExpand([
-              shouldExpand[0],
-              shouldExpand[1],
-              !shouldExpand[2],
-              shouldExpand[3],
-            ]);
-            setShouldHide([true, true, false, true]);
-            return null;
-          }}
-          color="#FFA500"
-          companyName="Sparted"
-          jobTitle="FullStack Developer"
-          jobDescription="Lots of work done"
-          shouldExpand={shouldExpand[2]}
-          shouldHide={shouldHide[2]}
-        />
-        <CustomJobCard
-          onClick={() => {
-            if (shouldExpand[3]) return reset();
-            setShouldExpand([
-              shouldExpand[0],
-              shouldExpand[1],
-              shouldExpand[2],
-              !shouldExpand[3],
-            ]);
-            setShouldHide([true, true, true, false]);
-            return null;
-          }}
-          color="#FF2253"
-          companyName="Freelancer"
-          jobTitle="FullStack Developer"
-          jobDescription="Lots of work done"
-          shouldExpand={shouldExpand[3]}
-          shouldHide={shouldHide[3]}
-        />
+        <ColumnContainer>
+          <CustomJobCard
+            displayModal={displayModal}
+            onClick={() => {
+              setDisplayModal(true);
+              setModalColor('#8BF896');
+            }}
+            color="#8BF896"
+            companyName="Tekyn"
+            jobTitle="Lead Developer"
+            jobDescription="Lots of work done"
+          />
+          <CustomJobCard
+            displayModal={displayModal}
+            onClick={() => {
+              setDisplayModal(true);
+              setModalColor('#e4ee89');
+            }}
+            color="#e4ee89"
+            companyName="TrackTL"
+            jobTitle="Frontend Developer"
+            jobDescription="Lots of work done"
+          />
+        </ColumnContainer>
+        <ColumnContainer>
+          <CustomJobCard
+            displayModal={displayModal}
+            onClick={() => {
+              setDisplayModal(true);
+              setModalColor('#FFA500');
+            }}
+            color="#FFA500"
+            companyName="Sparted"
+            jobTitle="FullStack Developer"
+            jobDescription="Lots of work done"
+          />
+          <CustomJobCard
+            displayModal={displayModal}
+            onClick={() => {
+              setDisplayModal(true);
+              setModalColor('#FF2253');
+            }}
+            color="#FF2253"
+            companyName="Freelancer"
+            jobTitle="FullStack Developer"
+            jobDescription="Lots of work done"
+          />
+        </ColumnContainer>
       </Grid>
+      <JobModal
+        onClick={() => setDisplayModal(false)}
+        color={modalColor}
+        displayModal={displayModal}
+        companyName="Freelancer"
+        jobTitle="FullStack Developer"
+        jobDescription="Lots of work done"
+      />
     </Container>
   );
 };
