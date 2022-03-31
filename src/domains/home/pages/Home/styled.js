2b1d8895dca0cbadc4747775170adf6bc366f8f7
Shaftwin <florian.champin.neault@gmail.com>
2022-03-31 22:33:46 +0200

    fix: Home very small height

--- src/domains/home/pages/Home/styled.js
+++ src/domains/home/pages/Home/styled.js
@@ -7,4 +7,9 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   background-color: ${({ theme }) => theme.backgroundColors.dark};
+  /*TODO Generate double scroll bar with very small screens  */
+  min-height: 700px;
+  @media screen and (min-height: 700px) {
+    min-height: unset;
+  }
 `;
